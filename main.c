04d5bf022b860c2df62de9ed49e2780009dfab79
zhuzhh <zhuzhh@mteroi.com>
2013-12-26 14:45:26 +0800

    test interface
    interface has been test.
    unfinished:
    1. moto driver function;
    2. LED and buzzer fuction;
    3. simplify the procedure;

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <C8051F120.h>                 // SFR declarations
 #include <stdio.h>
+#include <stdlib.h>
 #include <absacc.h>
 #include "global.h"
 #include "T6963.h"
@@ -14,24 +15,25 @@
 // MAIN Routine
 //-----------------------------------------------------------------------------
 void Run();
-void print_num(void);
+void print_num(char str[4][4]);
+void itoaTest(unsigned int num,char str[]);
 
-void key_test();
-void DA_test();
-void ADC_test();
-void Led_test();
-
-static	char param[4][4]={0,0,1,0,1,0,0,0,0,0,3,0,1,0,0,0};
+//void key_test();
+//void DA_test();
+//void ADC_test();
+//void Led_test();
 
 
 void main (void) 
 {
-	unsigned int i=0;
-	char t;
 
-	char run_status=0;
+	char t;
+	char param[4][4]={0,0,1,0,1,0,0,0,0,0,3,0,3,0,0,0};
+	bit run_status=0;
 	char cursor_pos=2;
-//	char m,n;
+	unsigned int cur_time1,cur_time2,cur_speed1,cur_speed2;
+	char m,n;
+	char temp_a[4][4];
 
 	WDTCN = 0xDE;                       // Disable Watchdog timer
 	WDTCN = 0xAD;
@@ -71,67 +73,79 @@ void main (void)
 	 Open_Cursor(11,3,7);					 //开启光标
  	
 	while(1)
-	{
-	 print_num();	  //更新设置参数
+	{	
+		if(run_status==0)
+		{
+			 print_num(param);	  //更新设置参数
+			 if(cursor_pos<8)
+			 	Open_Cursor(10+cursor_pos%4,3+cursor_pos/4,7);
+			 else Open_Cursor(10+cursor_pos%4,4+cursor_pos/4,7);
 
-	if(run_status==0)
-	{
-	 if(cursor_pos<8)
-	 	Open_Cursor(10+cursor_pos%4,3+cursor_pos/4,7);
-	 else Open_Cursor(10+cursor_pos%4,4+cursor_pos/4,7);
-	 }
-	 else NO_Cursor();
-	 t = PNL_30A_Key_get();
-	 if(run_status==0)	   //非运行状态
-	 {
-		 switch(t)
-		 {	//数字输入功能
-		 	case 2:param[cursor_pos/4][cursor_pos%4]=1;	break;	
-			case 3:param[cursor_pos/4][cursor_pos%4]=2; break;	
-			case 4:param[cursor_pos/4][cursor_pos%4]=3;	break;
-			case 8:param[cursor_pos/4][cursor_pos%4]=4;	break;
-			case 9:param[cursor_pos/4][cursor_pos%4]=5;	break;
-			case 10:param[cursor_pos/4][cursor_pos%4]=6;break;
-			case 14:param[cursor_pos/4][cursor_pos%4]=7;break;
-			case 15:param[cursor_pos/4][cursor_pos%4]=8;break;
-			case 16:param[cursor_pos/4][cursor_pos%4]=9;break;
-			case 21:param[cursor_pos/4][cursor_pos%4]=0;break;
-			//移动光标方向按键功能
-			case 22:if(cursor_pos==0)cursor_pos=15;else cursor_pos--;break;
-			case 24:if(cursor_pos==15)cursor_pos=0;else cursor_pos++;break;			
-			case 17:if(cursor_pos<4)cursor_pos=cursor_pos+12;else cursor_pos-=4;break;
-			case 23:if(cursor_pos>11)cursor_pos=cursor_pos-12;else cursor_pos+=4;break;
-			//
-			case '-':if(param[cursor_pos/4][cursor_pos%4]<9) param[cursor_pos/4][cursor_pos%4]++;break;
-			case '+':if(param[cursor_pos/4][cursor_pos%4]>0) param[cursor_pos/4][cursor_pos%4]--;break;
-			//启动执行按键
-			case 5:
-			case 18:
-			case 25:run_status=1;Run();break;	
-			default: break;				
-	
-		  }
-   	}
-	else		//运行状态，其他按键都失效，只有stop和Esc按键有效
-	{
-		  switch(t)
-		  {
-		  	case 6:
-			case 12:	run_status=0;break;
-			default: break;
+ 			 t = PNL_30A_Key_get(); 			
+			 switch(t)
+				 {	//数字输入功能
+				 	case 2:param[cursor_pos/4][cursor_pos%4]=1;if(cursor_pos<15)cursor_pos++;break;	
+					case 3:param[cursor_pos/4][cursor_pos%4]=2;if(cursor_pos<15)cursor_pos++;break;	
+					case 4:param[cursor_pos/4][cursor_pos%4]=3;if(cursor_pos<15)cursor_pos++;break;
+					case 8:param[cursor_pos/4][cursor_pos%4]=4;if(cursor_pos<15)cursor_pos++;break;
+					case 9:param[cursor_pos/4][cursor_pos%4]=5;if(cursor_pos<15)cursor_pos++;break;
+					case 10:param[cursor_pos/4][cursor_pos%4]=6;if(cursor_pos<15)cursor_pos++;break;
+					case 14:param[cursor_pos/4][cursor_pos%4]=7;if(cursor_pos<15)cursor_pos++;break;
+					case 15:param[cursor_pos/4][cursor_pos%4]=8;if(cursor_pos<15)cursor_pos++;break;
+					case 16:param[cursor_pos/4][cursor_pos%4]=9;if(cursor_pos<15)cursor_pos++;break;
+					case 21:param[cursor_pos/4][cursor_pos%4]=0;if(cursor_pos<15)cursor_pos++; break;
+					//移动光标方向按键功能
+					case 22:if(cursor_pos==0)cursor_pos=15;else cursor_pos--;break;
+					case 24:if(cursor_pos==15)cursor_pos=0;else cursor_pos++;break;			
+					case 17:if(cursor_pos<4)cursor_pos=cursor_pos+12;else cursor_pos-=4;break;
+					case 23:if(cursor_pos>11)cursor_pos=cursor_pos-12;else cursor_pos+=4;break;
+					//旋钮功能区
+					case '-':if(param[cursor_pos/4][cursor_pos%4]<9) param[cursor_pos/4][cursor_pos%4]++;break;
+					case '+':if(param[cursor_pos/4][cursor_pos%4]>0) param[cursor_pos/4][cursor_pos%4]--;break;
+					//启动执行按键
+					case 5:
+					case 18:
+					case 25:run_status=1;
+							cur_time1=cur_time2=0;
+							for(m=0;m<4;m++){cur_time1=cur_time1*10+param[0][m];cur_time2=cur_time2*10+param[2][m];}
+							for(m=0;m<4;m++)  for(n=0;n<4;n++)	temp_a[m][n]=param[m][n]; 
+							break;	
+					default: break;							
+				  }
+		}
+		else //run_status==1 	//执行阶段的程序
+			{
+			  NO_Cursor();
+			  itoaTest(cur_time1,temp_a[0]);	//实时更新剩余时间			  
+			  itoaTest(cur_time2,temp_a[2]);
+			  print_num(temp_a);
+			  if(cur_time1+cur_time2>0)		 //倒计时运行阶段
+			  {
+			  	delay_ms(1000);
+			  	if(cur_time1>0)
+			  		cur_time1--;
+				else cur_time2--;
+			  }
+			  if(cur_time1==0 && cur_time2==0 )   run_status=0;  //运行结束，判断退出
+					
+			  t = PNL_30A_Key_return(); 				//运行状态，其他按键都失效，只有stop和Esc按键有效
+			  switch(t)
+			  {
+			  	case 6:
+				case 12:	run_status=0;break;
+				default: break;
+			  }
+		   }
+     }
 
-		  }
-
-	}
-   }
 }
 
 //刷新输出设置参数
-void print_num(void)
+void print_num(char str[4][4])
 {
 	 char m,n;
 	 char state;
-	 char posi;
+
 	 for(m=0;m<4;m++)
 	 {
 	 if(m<2)
@@ -140,13 +154,14 @@ void print_num(void)
 	     state=0;
 		 for(n=0;n<4;n++)
 		 {
-		 	if(param[m][n]==0 && state==0)
+		 	if(str[m][n]==0 && state==0)
 		 		T6963_putchar(' ');
 		 	else 
 			{
-			T6963_putchar(param[m][n]+0x30);
+			T6963_putchar(str[m][n]+0x30);
 			state=1;
-			}			
+			}
+			if(n==2)state=1;			
 		 }
 	 }
 }
@@ -155,86 +170,85 @@ void print_num(void)
 void Run()
 {
 
-	PNL_30A_Buzzer_on();
-	delay_ms(200);
-	PNL_30A_Buzzer_off();
-
-
-}
-
-void ADC_test()
-{
-	float m;
-	char  t;
-	while(1)
-	{
-		ADC0_start(ADC_CH0);
-		m = ADC0_get()* 2.4300 / 4095;
-		T6963_printf("ADC_CH0 voltage:\n %1.3fV\n",m);
-		ADC0_start(ADC_CH1);
-		m = ADC0_get()* 2.4300 / 4095;
-		T6963_printf("ADC_CH1 voltage:\n %1.3fV\n",m);
-		ADC0_start(ADC_CH2);
-		m = ADC0_get()* 2.430 / 4095;
-		T6963_printf("ADC_CH2 voltage:\n %1.3fV\n",m);
-		ADC0_start(ADC_CH3);
-		m = ADC0_get()* 2.4300 / 4095;
-		T6963_printf("ADC_CH3 voltage:\n %1.3fV\n",m);
-		t =PNL_30A_Key_return();
-		if(t==12)break;
-		delay_ms(200);
-		T6963_lcd_back();
-	}
+//	PNL_30A_Buzzer_on();
+//	delay_ms(200);
+//	PNL_30A_Buzzer_off();
 
 }
 
-void key_test()
+void itoaTest(unsigned int num,char str[])
 {
-	char t;
-	while(1)
+
+unsigned char i;
+unsigned char temp[4];
+	for (i=0;i<4;i++)
 	{
-		t =PNL_30A_Key_get();
-		if(t==12)break;
-		T6963_printf("%bd",t);
+		str[3-i]=num%10;
+		num/=10;
 	}
 }
 
-void DA_test()
-{
-	char t;
-	int  f=100;
-	
-	while(1)
-	{
-		Set_DAC_Frequent(f);
-		T6963_printf("The frequent\n");
-		T6963_printf("    %02d    Hz",f);
-		t =PNL_30A_Key_get();
-		if(t==12)break;
-		else if(t=='+')
-			f+=10;
-		else if(t=='-')
-			f-=10;
-		T6963_lcd_back();
-	}	
-}
+//void ADC_test()
+//{
+//	float m;
+//	char  t;
+//	while(1)
+//	{
+//		ADC0_start(ADC_CH0);
+//		m = ADC0_get()* 2.4300 / 4095;
+//		T6963_printf("ADC_CH0 voltage:\n %1.3fV\n",m);
+//		ADC0_start(ADC_CH1);
+//		m = ADC0_get()* 2.4300 / 4095;
+//		T6963_printf("ADC_CH1 voltage:\n %1.3fV\n",m);
+//		ADC0_start(ADC_CH2);
+//		m = ADC0_get()* 2.430 / 4095;
+//		T6963_printf("ADC_CH2 voltage:\n %1.3fV\n",m);
+//		ADC0_start(ADC_CH3);
+//		m = ADC0_get()* 2.4300 / 4095;
+//		T6963_printf("ADC_CH3 voltage:\n %1.3fV\n",m);
+//		t =PNL_30A_Key_return();
+//		if(t==12)break;
+//		delay_ms(200);
+//		T6963_lcd_back();
+//	}
+//
+//}
 
-void Led_test()
-{
-	char t;
-	char i=1;
-	while(1)
-	{
-		PNL_30A_Led_set(i);
-		t =PNL_30A_Key_return();
-		if(t==12)break;
-		i<<=1;
-		if(i%0xf==0)i=1;	
-	}		
-}
 
-void Buzzer_test()
-{
+//
+//void key_test()
+//{
+//	char t;
+//	while(1)
+//	{
+//		t =PNL_30A_Key_get();
+//		if(t==12)break;
+//		T6963_printf("%bd",t);
+//	}
+//}
+//
+//void DA_test()
+//{
+//	char t;
+//	int  f=100;
+//	
+//	while(1)
+//	{
+//		Set_DAC_Frequent(f);
+//		T6963_printf("The frequent\n");
+//		T6963_printf("    %02d    Hz",f);
+//		t =PNL_30A_Key_get();
+//		if(t==12)break;
+//		else if(t=='+')
+//			f+=10;
+//		else if(t=='-')
+//			f-=10;
+//		T6963_lcd_back();
+//	}	
+//}
+//
+//void Led_test()
+//{
 //	char t;
 //	char i=1;
 //	while(1)
@@ -242,10 +256,24 @@ void Buzzer_test()
 //		PNL_30A_Led_set(i);
 //		t =PNL_30A_Key_return();
 //		if(t==12)break;
-//		i<<1;
+//		i<<=1;
 //		if(i%0xf==0)i=1;	
 //	}		
-}
+//}
+
+//void Buzzer_test()
+//{
+////	char t;
+////	char i=1;
+////	while(1)
+////	{
+////		PNL_30A_Led_set(i);
+////		t =PNL_30A_Key_return();
+////		if(t==12)break;
+////		i<<1;
+////		if(i%0xf==0)i=1;	
+////	}		
+//}
 //-----------------------------------------------------------------------------
 // End Of File
 //-----------------------------------------------------------------------------
